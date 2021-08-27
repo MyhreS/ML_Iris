cfb2b386410e38f543135bb54a2b91da69b966ed
Simon Myhre <56484055+SimonMyhre@users.noreply.github.com>
2021-08-27 23:46:41 +0200

    Updated url attribute to be the downloaded iris.csv instead. Mac had problems with getting the url

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+
+url = "iris.csv"
 names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "class"]
 dataset = read_csv(url, names=names)
 
@@ -25,4 +26,4 @@ dataset = read_csv(url, names=names)
 array = dataset.values
 X = array[:,0:4]
 y = array[:,4]
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
